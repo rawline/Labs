c57819772e973856b85af91fbb0d3e61ee02df47
Rafael <rawoline@gmail.com>
2023-12-07 20:07:26 +0300

    lab 2

---
+++ lab2v2/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2v2
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
---
+++ lab2v2/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace lab2v2
+{
+    internal class Program
+    {
+        static NoteBook notebook = new NoteBook();
+
+        static void Main(string[] args)
+        {
+            int choice;
+            do
+            {
+                ShowMenu();
+                choice = GetUserChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        notebook.ViewAllContacts();
+                        break;
+                    case 2:
+                        SearchOptions();
+                        break;
+                    case 3:
+                        notebook.AddNewContact();
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+            } while (choice != 10);
+        }
+
+        static void ShowMenu()
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. View all contacts");
+            Console.WriteLine("2. Search");
+            Console.WriteLine("3. New contact");
+            Console.WriteLine("4. Exit");
+            Console.Write("> ");
+        }
+
+        static int GetUserChoice()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                Console.Write("> ");
+            }
+            return choice;
+        }
+
+        static void SearchOptions()
+        {
+            Console.WriteLine("Search by");
+            Console.WriteLine("1. Name");
+            Console.WriteLine("2. Surname");
+            Console.WriteLine("3. Name and Surname");
+            Console.WriteLine("4. Phone");
+            Console.WriteLine("5. E-mail");
+            Console.Write("> ");
+
+            notebook.SearchContacts(GetUserChoice());
+        }
+    }
+}
